5aa271e43636cde55f13d0981d61dcbcdb55c50b
ChumboMJ <tforste@gmail.com>
2024-12-28 22:29:36 -0800

    Update the url to be the 2024 AX1 PAX results

--- ApexScraper-pax.py
+++ ApexScraper-pax.py
@@ -4,6 +4,6 @@ import json
 
 # Script used for scraping pax event data
 
-url = 'https://www.wmclub.org/wp-content/uploads/2024/03/AX1-winter-break-031724_fin.htm'
+url = 'https://www.wmclub.org/wp-content/uploads/2024/03/AX1-winter-break-031724_pax.htm'
 
 print(url)
