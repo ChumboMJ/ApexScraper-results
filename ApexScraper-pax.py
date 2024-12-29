f57ee63fadfb924f91c680bc9fcedae2daa7d5d7
ChumboMJ <tforste@gmail.com>
2024-12-28 22:26:55 -0800

    Create new script for scraping PAX results for events

---
+++ ApexScraper-pax.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+# Script used for scraping pax event data
+
+url = 'https://www.wmclub.org/wp-content/uploads/2024/03/AX1-winter-break-031724_fin.htm'
+
+print(url)
