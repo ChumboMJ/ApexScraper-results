14cb64fd74b4f4dc47bda527eefb2c1c27602da6
ChumboMJ <tforste@gmail.com>
2024-12-28 22:33:53 -0800

    Send a GET to the provided url and print an indicator if the Request was sucessful or unsuccessful

--- ApexScraper-pax.py
+++ ApexScraper-pax.py
@@ -11,5 +11,12 @@ request_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
+# Use a GET request to retrieve the site page
+response = requests.get(url, headers=request_headers)
+
+if response.status_code == 200:
+    print("Request was a success!")
+else:
+    print("Request was unsucessful :(")
 
 print(url)
