6d93731a36685ce2791c4f3399a2613b156a6212
ChumboMJ <tforste@gmail.com>
2024-12-28 22:30:32 -0800

    Set up headers to mimic a browser request

--- ApexScraper-pax.py
+++ ApexScraper-pax.py
@@ -6,4 +6,10 @@ import json
 
 url = 'https://www.wmclub.org/wp-content/uploads/2024/03/AX1-winter-break-031724_pax.htm'
 
+# Set headers to mimic a browser request
+request_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+
 print(url)
